c7d07c93e54feb12c10167c01163d2f340fad49b
Pedro Rojas <pedrofrojasu@gmail.com>
2024-01-07 22:50:56 +0100

    Working basic version

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.wo.relativenumber = true
+vim.g.neovide_fullscreen = true
+os.execute("setxkbmap -option caps:escape")
+vim.g.neovide_transparency = 0.6
+vim.api.nvim_set_keymap('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
+vim.g.neovide_hide_mouse_when_typing = true
+
 return {}
