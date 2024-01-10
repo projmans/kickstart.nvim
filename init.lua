f577bf1420387bb8c6789b995fcd8b6df66e9a1e
Pedro Rojas <pedrofrojasu@gmail.com>
2024-01-10 22:39:06 +0100

    Add func defaults to avoid warnings

--- init.lua
+++ init.lua
@@ -424,7 +424,9 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
-
+    modules = {},
+    sync_install = false,
+    ignore_install = {},
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
 
