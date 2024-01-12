baf3f169b2ec91b16ca3b5dd7da9bbbcdc6e44b8
Pedro Rojas <pedrofrojasu@gmail.com>
2024-01-12 23:48:34 +0100

    Add missing fields to remove warnings

--- init.lua
+++ init.lua
@@ -424,11 +424,14 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
-    modules = {},
-    sync_install = false,
-    ignore_install = {},
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- List of parsers to ignore installing
+    ignore_install = {},
+    -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+    modules = {},
 
     highlight = { enable = true },
     indent = { enable = true },
