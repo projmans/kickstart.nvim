7a9a630310d872dcdfe3fbdac73a440b0cb76e71
Pedro Rojas <pedrofrojasu@gmail.com>
2024-01-14 22:14:36 +0100

    Add inlay hint line in prep for v0.10

--- init.lua
+++ init.lua
@@ -585,6 +585,7 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      -- hint = { enable = true },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
     },
