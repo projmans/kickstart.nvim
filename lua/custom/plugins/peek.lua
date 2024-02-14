0072f5ebfc20d1da27865df57f66d9f03d874fee
Pedro Rojas <pedrofrojasu@gmail.com>
2024-02-14 20:26:10 +0100

    Add fresh changes

---
+++ lua/custom/plugins/peek.lua
@@ -0,0 +1,39 @@
+-- Peek config --
+
+return {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  config = function()
+    local peek = require('peek')
+
+    -- vim.api.nvim_create_user_command('PeekOpen', function()
+    --   if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+    --     vim.fn.system('i3-msg split horizontal')
+    --     peek.open()
+    --   end
+    -- end, {})
+    --
+    -- vim.api.nvim_create_user_command('PeekClose', function()
+    --   if peek.is_open() then
+    --     peek.close()
+    --     vim.fn.system('i3-msg move left')
+    --   end
+    -- end, {})
+
+    peek.setup(
+      {
+        auto_load = true,          -- whether to automatically load preview when
+        close_on_bdelete = true,   -- close preview window on buffer delete
+        syntax = true,             -- enable syntax highlighting, affects performance
+        theme = 'dark',            -- 'dark' or 'light'
+        update_on_change = true,
+        app = 'webview',           -- 'webview', 'browser', string or a table of strings
+        filetype = { 'markdown' }, -- list of filetypes to recognize as markdown relevant if update_on_change is true
+        throttle_at = 200000,      -- start throttling when file exceeds this amount of bytes in size
+        throttle_time = 'auto',    -- minimum amount of time in milliseconds that has to pass before starting new render
+      })
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+}
